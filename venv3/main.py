767144fb4a9b991d82bfeb382083d34597032e63
user <q@q.q>
2025-01-23 11:22:28 +0300

    1

---
+++ venv3/main.py
@@ -0,0 +1,24 @@
+
+class MyClass:
+    def __init__(self, param):
+        self.param = param
+
+    def print_param(self):
+        print(self.param+1)
+
+
+my_instance = MyClass(5)
+my_instance.print_param()  # Вывод: 5
+
+def my_function():
+    q = 1
+    print(q)
+
+
+
+
+
+
+
+
+
